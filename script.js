e18507144a5ed03e6b081cc599c4d21761bfd08f
suraxj <prakashsinghsuraj69@gmail.com>
2025-06-29 10:00:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const sideMenu = document.querySelector('#sideMenu');
+const navBar = document.querySelector('nav');
+const navLinks = document.querySelector("nav ul");
+
+function openMenu() {
+    sideMenu.style.transform = 'translateX(-16rem)';
+}
+function closeMenu() {
+    sideMenu.style.transform = 'translateX(16rem)';
+}
+
+window.addEventListener('scroll', () => {
+    if(scroll > 50){
+         navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg',
+            'shadow-sm');
+            navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50',
+            );
+    }else{
+            navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg',
+            'shadow-sm');
+            navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50',
+            ); 
+         }
+        
+
+})
+
+// -----light mode and dark mode----
+
+  if (localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+        document.documentElement.classList.add("dark")
+    }else{
+        document.documentElement.classList.remove("dark");
+    }
+    function toggleTheme() {
+        document.documentElement.classList.toggle("dark");
+        if (document.documentElement.classList.contains("dark")) {
+            localStorage.theme = "dark";
+        } else {
+            localStorage.theme = "light";
+        }
+    }
